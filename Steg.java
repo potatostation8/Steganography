b4091283ee79455425bdf39da64757bb3a7bfeb0
potatostation8 <zacharytbarr@gmail.com>
2015-05-27 14:03:36 -0400

    Added imports and image display
    Rather important additions.

--- Steg.java
+++ Steg.java
@@ -1,5 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+
 public class Steg {
   public static void main (String [] args) {
-  
+    BufferedImage sample = null;		
+		try {
+			sample = ImageIO.read (new File("./sample.bmp"));
+		} catch (IOException e) {};
+		
+		if (sample == null) {
+			System.out.println ("Error loading file.");
+			return;
+		}
+		
+		int w = sample.getWidth();
+		int h = sample.getHeight();
+		
+		for (int x = 0; x < w; x++) {
+			for (int y = 0; y < h; y++) {
+				Color c = new Color (sample.getRGB(x,y));
+				
+				System.out.print ("Pixel at " + x + ", " + y + ": ");
+				System.out.print ("R " + c.getRed() + ", ");			
+				System.out.print ("G " + c.getGreen() + ", ");
+				System.out.println ("B " + c.getBlue() );
+			}		
+		}
   }
 }  
